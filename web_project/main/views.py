18627483c06ff6cfab6d48bb94b50e37a1693362
Vadim <vadim@inbox.com>
2023-09-14 19:52:38 +0300

    add def about

--- web_project/main/views.py
+++ web_project/main/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
     return HttpResponse('<h4>Я поддерживаю я</h4>')
+
+def about():
+    return HttpResponse('<h4>Я есть в репозиториях, а ты?</h4>')
