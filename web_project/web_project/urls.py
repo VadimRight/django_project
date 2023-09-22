0503736a022020eede83108d4fb4b2443ea62bad
vadim1vadim2vadim3 <vadim.komashko@bk.ru>
2023-09-22 10:28:43 +0300

    fixed templates in soft_skills

--- web_project/web_project/urls.py
+++ web_project/web_project/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('soft_skills/', include('soft_skills.urls'))
+    path('soft_skills/', include('soft_skills.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
