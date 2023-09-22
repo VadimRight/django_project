13847c7b17272d79a30b794096886c56c8a1605a
vadim1vadim2vadim3 <vadim.komashko@bk.ru>
2023-09-22 20:52:14 +0300

    Created hard_skills app, settings, templates  for app are done, page is working correctly

--- web_project/web_project/settings.py
+++ web_project/web_project/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'soft_skills',
+    'hard_skills',
 ]
 
 MIDDLEWARE = [
--- web_project/web_project/urls.py
+++ web_project/web_project/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('soft_skills/', include('soft_skills.urls')),
+    path('hard_skills/', include('hard_skills.urls')),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
