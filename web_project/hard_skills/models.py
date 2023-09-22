72793bb00d63b53d07316cdb02436c23ef17b251
vadim1vadim2vadim3 <vadim.komashko@bk.ru>
2023-09-22 21:39:20 +0300

    Hrad-Skills page is working

--- web_project/hard_skills/models.py
+++ web_project/hard_skills/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField('Характеристика', max_length= 50)
+    description = models.CharField('Описание', max_length= 250)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name ='Характеристика'
+        verbose_name_plural = 'Характеристики'
--- web_project/hard_skills/views.py
+++ web_project/hard_skills/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
-# from .models import Articles
+from .models import Articles
 
 
 def hard_skills(request):
-    # hard_skills = Articles.objects.all()
+    hard_skills = Articles.objects.all()
     return render(request, 'hard_skills/hard_skills.html', {'hard_skills': hard_skills})
 
