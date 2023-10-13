b7b69117e0798ee395d5261fe6c095f44208232a
vadimright <vadim.komashko@bk.ru>
2023-10-13 10:26:30 +0300

    Removed Redundant Static File Calls

--- web_project/hard_skills/urls.py
+++ web_project/hard_skills/urls.py
@@ -6,4 +6,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.hard_skills, name='hard_skills'),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    ]
--- web_project/soft_skills/urls.py
+++ web_project/soft_skills/urls.py
@@ -6,4 +6,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.soft_skills, name='soft_skills'),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    ]
