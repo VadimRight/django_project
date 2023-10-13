7a7b2c5eb11a8cbcafa43460b25ed56979fe7fac
vadimright <vadim.komashko@bk.ru>
2023-10-13 15:08:23 +0300

    Removed References to Nonexistent Links

--- web_project/main/urls.py
+++ web_project/main/urls.py
@@ -5,6 +5,5 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('about', views.about, name='about')
+    path('', views.index, name='home')
     ]
